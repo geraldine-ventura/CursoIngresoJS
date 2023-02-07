221032d0f981b6f7064be660b82e4e7da9ab4b24
geraldine-ventura <115373615+geraldine-ventura@users.noreply.github.com>
2023-02-07 20:49:04 -0300

    ejerci if hasta el 10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,29 @@
+
+/*Enunciado:
+Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4*/
+
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+let nota;
+//let maximo=10;
+//let minimo=1;
+//numero= Math.round(Math.random()* (maximo-minimo) + minimo);
+nota= Math.round(Math.random()* (9) + 1);
+alert(nota);
+if (nota<4){
+	alert("Vamos, la proxima se puede");
+}
+else if (nota<9){
+		alert("aprobo");
+}
+else {
+		alert("exelente");
+}
+}
 
-}//FIN DE LA FUNCIÓN
+ 
+
+//FIN DE LA FUNCIÓN
